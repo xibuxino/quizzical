68e0697383ac8852fb67a5a91993a3e12c699a0c
Ola <xibuxino@gmail.com>
2025-07-25 13:40:43 +0200

    feat: reorganize structure and add starter component

---
+++ src/components/Start.jsx
@@ -0,0 +1,15 @@
+export default function Start() {
+	return (
+		<section className='start'>
+			<h1 className='start-title'>Quizzical</h1>
+			<p className='start-description'>
+				Challenge yourself with quick, engaging quizzes across various topics
+				and see how much you really know. Ready to prove yourself? Start your
+				first quiz now!
+			</p>
+			<button className='start-button' type='button'>
+				Start quiz
+			</button>
+		</section>
+	);
+}
