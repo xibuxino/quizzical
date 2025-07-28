4f1568b889ec9250c44ec3b646b3aa622269b8c7
Ola <xibuxino@gmail.com>
2025-07-28 15:03:43 +0200

    changed path for svg files

--- src/App.jsx
+++ src/App.jsx
@@ -116,11 +116,8 @@ function App() {
 	// return
 	return (
 		<>
-			<img
-				className='bg-svg-item blob-yellow'
-				src='./src/assets/yellow_blob.svg'
-			/>
-			<img className='bg-svg-item blob-blue' src='./src/assets/blue_blob.svg' />
+			<img className='bg-svg-item blob-yellow' src='./yellow_blob.svg' />
+			<img className='bg-svg-item blob-blue' src='./blue_blob.svg' />
 
 			<main>
 				{!isRun && (
